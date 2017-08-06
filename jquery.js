0e891c8b14367e48a5a953b1fa50b614e16f599e
Dennis George <dennisgeorge@Denniss-MacBook-Pro.local>
2017-08-06 13:42:40 -0400

    changed social media links to proper links

--- jquery.js
+++ jquery.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
     //settings for slider
     var width = 720;
     var animationSpeed = 1000;
-    var pause = 5000;
+    var pause = 3000;
     var currentSlide = 1;
 
     //cache DOM elements
