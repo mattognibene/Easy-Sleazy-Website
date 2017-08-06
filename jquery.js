a38b1bf700dae5d00b1857f33a10ad04b8968d53
Dennis George <dennisgeorge@Denniss-MacBook-Pro.local>
2017-08-05 22:01:48 -0400

    added slider to be configured. Added pics to Design page

--- jquery.js
+++ jquery.js
@@ -1,20 +1,50 @@
 $(document).ready(function(){	
-	/*
-	$('.navigation').mouseenter(function(){
-		$('.navigation').fadeTo('fast', 1);
-	});
-	$('.navigation').mouseleave(function(){
-		$('.navigation').fadeTo('fast', .5);
-		$('#home').css('background','rgba(255,255,255,1)');
-		//TODO
-	});
-	*/
-	//TODO find a better way to do this
+
 	$('li').mouseenter(function(){
 		$(this).css('color', 'black');
 	});
 	$('li').mouseleave(function(){
 		$(this).css('color', 'grey');
 	});	
+	'use strict';
+
+$(function() {
+
+    //settings for slider
+    var width = 720;
+    var animationSpeed = 1000;
+    var pause = 5000;
+    var currentSlide = 1;
+
+    //cache DOM elements
+    var $slider = $('#slider');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.slide', $slider);
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    $slideContainer
+        .on('mouseenter', pauseSlider)
+        .on('mouseleave', startSlider);
+
+    startSlider();
+
+
+});
+
 	
 });
